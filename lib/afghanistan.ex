a56bae8b5a2417a4c36e168915a4b35eedd8b746
Clivern <hello@clivern.com>
2022-08-04 00:59:01 +0200

    init

--- lib/afghanistan.ex
+++ lib/afghanistan.ex
@@ -3,4 +3,47 @@
 # license that can be found in the LICENSE file.
 
 defmodule Terrier.Country.Afghanistan do
+  def get_cities() do
+    %{
+      badakhshan: "Badakhshan",
+      badghis: "Badghis",
+      baghlan: "Baghlan",
+      balkh: "Balkh",
+      bamyan: "Bamyan",
+      daykundi: "Daykundi",
+      farah: "Farah",
+      faryab: "Faryab",
+      ghazni: "Ghazni",
+      ghor: "Ghor",
+      helmand: "Helmand",
+      herat: "Herat",
+      jowzjan: "Jowzjan",
+      kabul: "Kabul",
+      kandahar: "Kandahar",
+      kapisa: "Kapisa",
+      khost: "Khost",
+      kunar: "Kunar",
+      kunduz: "Kunduz",
+      laghman: "Laghman",
+      logar: "Logar",
+      nangarhar: "Nangarhar",
+      nimruz: "Nimruz",
+      nuristan: "Nuristan",
+      paktika: "Paktika",
+      paktia: "Paktia",
+      panjshir: "Panjshir",
+      parwan: "Parwan",
+      damangan: "Samangan",
+      darepol: "Sar-e Pol",
+      takhar: "Takhar",
+      uruzgan: "Uruzgan",
+      wardak: "Wardak",
+      zabul: "Zabul"
+    }
+  end
+
+  def validate(name) do
+    values = get_cities() |> Map.values()
+    name in values
+  end
 end
--- lib/panama.ex
+++ lib/panama.ex
@@ -3,4 +3,12 @@
 # license that can be found in the LICENSE file.
 
 defmodule Terrier.Country.Panama do
+  def get_cities() do
+    %{}
+  end
+
+  def validate(name) do
+    values = get_cities() |> Map.values()
+    name in values
+  end
 end
